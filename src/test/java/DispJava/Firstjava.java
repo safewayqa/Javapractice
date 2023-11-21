1016817c60e925591bb877c9be1923e774db1747
rafi527@gmail.com <rafi527@gmail.com>
2023-11-21 12:52:25 +0530

    Tird chnages

---
+++ src/test/java/DispJava/Firstjava.java
@@ -0,0 +1,13 @@
+package DispJava;
+
+public class Firstjava {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Rafi Welcome");
+		System.out.println("Nargees welcome");
+		System.out.println("Rehan");
+	}
+
+}
